d49755a1551b92afe6b47dc54c8f00167de8ae45
Aaron <aaron.l.france@gmail.com>
2013-06-02 17:41:07 +0200

    Added logging.

--- src/simple_cache.erl
+++ src/simple_cache.erl
@@ -5,13 +5,16 @@
 insert(Key, Value) ->
 	case sc_store:lookup(Key) of
 		{ok, Pid} ->
+			sc_event:replace(Key, Value),
 			sc_element:replace(Pid, Value);
 		{error, _} ->
+			sc_event:create(Key, Value),
 			{ok, Pid} = sc_element:create(Value),
 			sc_store:insert(Key, Pid)
 	end.
 
 lookup(Key) ->
+	sc_event:lookup(Key),
 	try
 		{ok, Pid} = sc_store:lookup(Key),
 		sc_element:fetch(Pid)
@@ -21,6 +24,7 @@ lookup(Key) ->
 	end.
 
 delete(Key) ->
+	sc_event:delete(Key),
 	case sc_store:lookup(Key) of
 		{ok, Pid} ->
 			sc_store:delete(Pid);
