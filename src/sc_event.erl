c9d0f9ea8a278155260be1cb409e1afca5bbd96a
Aaron <aaron.l.france@gmail.com>
2013-06-02 17:41:29 +0200

    Added the gen_event module and it's accompanying event_handler module.

---
+++ src/sc_event.erl
@@ -0,0 +1,61 @@
+%%%-------------------------------------------------------------------
+%%% File    : sc_event.erl
+%%% Author  :  <xeno@localhost>
+%%% Description :
+%%%
+%%% Created :  2 Jun 2013 by  <xeno@localhost>
+%%%-------------------------------------------------------------------
+-module(sc_event).
+
+%%--------------------------------------------------------------------
+%% Include files
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% External exports
+-export([start_link/0,
+		 add_handler/2,
+		 delete_handler/2,
+		 lookup/1,
+		 create/2,
+		 replace/2,
+		 delete/1]).
+
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% External functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link/0
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start_link() ->
+	gen_event:start_link({local, ?SERVER}).
+
+%%--------------------------------------------------------------------
+%% Function: add_handler/0
+%% Description: Adds an event handler
+%%--------------------------------------------------------------------
+add_handler(Handler, Args) ->
+	gen_event:add_handler(?SERVER, Handler, Args).
+
+%%--------------------------------------------------------------------
+%% Function: delete_handler/2
+%% Description: Deletes an event handler
+%%--------------------------------------------------------------------
+delete_handler(Handler, Args) ->
+	gen_event:delete_handler(?SERVER, Handler, Args).
+
+lookup(Key) ->
+	gen_event:notify(?SERVER, {lookup, Key}).
+
+create(Key, Value) ->
+	gen_event:notify(?SERVER, {create, {Key, Value}}).
+
+replace(Key, Value) ->
+	gen_event:notify(?SERVER, {replace, {Key, Value}}).
+
+delete(Key) ->
+	gen_event:notify(?SERVER, {delete, Key}).
---
+++ src/sc_event_handler.erl
@@ -0,0 +1,118 @@
+%%%-------------------------------------------------------------------
+%%% File    : sc_event_handler.erl
+%%% Author  :  <xeno@localhost>
+%%% Description : 
+%%%
+%%% Created :  2 Jun 2013 by  <xeno@localhost>
+%%%-------------------------------------------------------------------
+-module(sc_event_handler).
+
+-behaviour(gen_event).
+%%--------------------------------------------------------------------
+%% Include files
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% External exports
+-export([start_link/0, add_handler/0, delete_handler/0]).
+
+%% gen_event callbacks
+-export([init/1, handle_event/2, handle_call/2, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {}).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% External functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link/0
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start_link() ->
+	gen_event:start_link({local, ?SERVER}). 
+
+%%--------------------------------------------------------------------
+%% Function: add_handler/0
+%% Description: Adds an event handler
+%%--------------------------------------------------------------------
+add_handler() ->
+	sc_event:add_handler(?MODULE, []).
+
+%%--------------------------------------------------------------------
+%% Function: delete_handler/0
+%% Description: Deletes an event handler
+%%--------------------------------------------------------------------
+delete_handler() ->
+	sc_event:delete_handler(?MODULE, []).
+
+%%====================================================================
+%% Server functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Func: init/1
+%% Returns: {ok, State}          |
+%%          Other
+%%--------------------------------------------------------------------
+init([]) ->
+	{ok, #state{}}.
+
+%%--------------------------------------------------------------------
+%% Func: handle_event/2
+%% Returns: {ok, State}                                |
+%%          {swap_handler, Args1, State1, Mod2, Args2} |
+%%          remove_handler                              
+%%--------------------------------------------------------------------
+handle_event({create, {Key, Value}}, State) ->
+	error_logger:info_msg("create(~w, ~w)~n", [Key, Value]),
+	{ok, State};
+handle_event({replace, {Key, Value}}, State) ->
+	error_logger:info_msg("replace(~w, ~w)~n", [Key, Value]),
+	{ok, State};
+handle_event({lookup, Key}, State) ->
+	error_logger:info_msg("lookup(~w)~n", [Key]),
+	{ok, State};
+handle_event({delete, Key}, State) ->
+	error_logger:info_msg("delete(~w)~n", [Key]),
+	{ok, State}.
+
+
+%%--------------------------------------------------------------------
+%% Func: handle_call/2
+%% Returns: {ok, Reply, State}                                |
+%%          {swap_handler, Reply, Args1, State1, Mod2, Args2} |
+%%          {remove_handler, Reply}                            
+%%--------------------------------------------------------------------
+handle_call(Request, State) ->
+	Reply = ok,
+	{ok, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% Func: handle_info/2
+%% Returns: {ok, State}                                |
+%%          {swap_handler, Args1, State1, Mod2, Args2} |
+%%          remove_handler                              
+%%--------------------------------------------------------------------
+handle_info(Info, State) ->
+	{ok, State}.
+
+%%--------------------------------------------------------------------
+%% Func: terminate/2
+%% Purpose: Shutdown the server
+%% Returns: any
+%%--------------------------------------------------------------------
+terminate(Reason, State) ->
+	ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%%--------------------------------------------------------------------
+code_change(OldVsn, State, Extra) ->
+	{ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
