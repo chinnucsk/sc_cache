b96e4daa6408dbe1f2696aed051afcd88bf7210b
Aaron <aaron.l.france@gmail.com>
2013-06-17 20:30:48 +0200

    Whitespace fix sc_event.erl.

--- src/sc_event.erl
+++ src/sc_event.erl
@@ -14,12 +14,12 @@
 %%--------------------------------------------------------------------
 %% External exports
 -export([start_link/0,
-		 add_handler/2,
-		 delete_handler/2,
-		 lookup/1,
-		 create/2,
-		 replace/2,
-		 delete/1]).
+	 add_handler/2,
+	 delete_handler/2,
+	 lookup/1,
+	 create/2,
+	 replace/2,
+	 delete/1]).
 
 
 -define(SERVER, ?MODULE).
@@ -32,30 +32,30 @@
 %% Description: Starts the server
 %%--------------------------------------------------------------------
 start_link() ->
-	gen_event:start_link({local, ?SERVER}).
+    gen_event:start_link({local, ?SERVER}).
 
 %%--------------------------------------------------------------------
 %% Function: add_handler/0
 %% Description: Adds an event handler
 %%--------------------------------------------------------------------
 add_handler(Handler, Args) ->
-	gen_event:add_handler(?SERVER, Handler, Args).
+    gen_event:add_handler(?SERVER, Handler, Args).
 
 %%--------------------------------------------------------------------
 %% Function: delete_handler/2
 %% Description: Deletes an event handler
 %%--------------------------------------------------------------------
 delete_handler(Handler, Args) ->
-	gen_event:delete_handler(?SERVER, Handler, Args).
+    gen_event:delete_handler(?SERVER, Handler, Args).
 
 lookup(Key) ->
-	gen_event:notify(?SERVER, {lookup, Key}).
+    gen_event:notify(?SERVER, {lookup, Key}).
 
 create(Key, Value) ->
-	gen_event:notify(?SERVER, {create, {Key, Value}}).
+    gen_event:notify(?SERVER, {create, {Key, Value}}).
 
 replace(Key, Value) ->
-	gen_event:notify(?SERVER, {replace, {Key, Value}}).
+    gen_event:notify(?SERVER, {replace, {Key, Value}}).
 
 delete(Key) ->
-	gen_event:notify(?SERVER, {delete, Key}).
+    gen_event:notify(?SERVER, {delete, Key}).
