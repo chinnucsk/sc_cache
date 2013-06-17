653427b5e1abc70e4a434bbe8a4c6ca4d63793e1
Aaron <aaron.l.france@gmail.com>
2013-06-17 20:27:02 +0200

    Whitespace fix sc_element.erl.

--- src/sc_element.erl
+++ src/sc_element.erl
@@ -17,13 +17,13 @@
 %%--------------------------------------------------------------------
 %% External exports
 -export([
-		 start_link/2,
-		 create/2,
-		 create/1,
-		 fetch/1,
-		 replace/2,
-		 delete/1
-		]).
+	 start_link/2,
+	 create/2,
+	 create/1,
+	 fetch/1,
+	 replace/2,
+	 delete/1
+	]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
@@ -38,22 +38,22 @@
 %% Description: Starts the server
 %%--------------------------------------------------------------------
 start_link(Value, LeaseTime) ->
-	gen_server:start_link(?MODULE, [Value, LeaseTime], []).
+    gen_server:start_link(?MODULE, [Value, LeaseTime], []).
 
 create(Value) ->
-	create(Value, ?DEFAULT_LEASE_TIME).
+    create(Value, ?DEFAULT_LEASE_TIME).
 
 create(Value, Timeout) ->
-	sc_element_sup:start_child(Value, Timeout).
+    sc_element_sup:start_child(Value, Timeout).
 
 fetch(Pid) ->
-	gen_server:call(Pid, fetch).
+    gen_server:call(Pid, fetch).
 
 delete(Pid) ->
-	gen_server:cast(Pid, delete).
+    gen_server:cast(Pid, delete).
 
 replace(Pid, Value) ->
-	gen_server:cast(Pid, {replace, Value}).
+    gen_server:cast(Pid, {replace, Value}).
 
 %%====================================================================
 %% Server functions
@@ -68,12 +68,12 @@ replace(Pid, Value) ->
 %%          {stop, Reason}
 %%--------------------------------------------------------------------
 init([Value, LeaseTime]) ->
-	Now = calendar:local_time(),
-	StartTime = calendar:datetime_to_gregorian_seconds(Now),
-	{ok, #state{value = Value,
-			   lease_time = LeaseTime,
-			   start_time = StartTime},
-			   time_left(StartTime, LeaseTime)}.
+    Now = calendar:local_time(),
+    StartTime = calendar:datetime_to_gregorian_seconds(Now),
+    {ok, #state{value = Value,
+		lease_time = LeaseTime,
+		start_time = StartTime},
+     time_left(StartTime, LeaseTime)}.
 
 %%--------------------------------------------------------------------
 %% Function: handle_call/3
@@ -86,11 +86,11 @@ init([Value, LeaseTime]) ->
 %%          {stop, Reason, State}            (terminate/2 is called)
 %%--------------------------------------------------------------------
 handle_call(fetch, _From, State) ->
-	#state{value = Value,
-		   lease_time = LeaseTime,
-		   start_time = StartTime} = State,
-	TimeLeft = time_left(StartTime, LeaseTime),
-	{reply, {ok, Value}, State, TimeLeft}.
+    #state{value = Value,
+	   lease_time = LeaseTime,
+	   start_time = StartTime} = State,
+    TimeLeft = time_left(StartTime, LeaseTime),
+    {reply, {ok, Value}, State, TimeLeft}.
 %%--------------------------------------------------------------------
 %% Function: handle_cast/2
 %% Description: Handling cast messages
@@ -99,12 +99,12 @@ handle_call(fetch, _From, State) ->
 %%          {stop, Reason, State}            (terminate/2 is called)
 %%--------------------------------------------------------------------
 handle_cast({replace, Value}, State) ->
-	#state{lease_time = LeaseTime,
-		   start_time = StartTime} = State,
-	TimeLeft = time_left(StartTime, LeaseTime),
-	{noreply, State#state{value = Value}, TimeLeft};
+    #state{lease_time = LeaseTime,
+	   start_time = StartTime} = State,
+    TimeLeft = time_left(StartTime, LeaseTime),
+    {noreply, State#state{value = Value}, TimeLeft};
 handle_cast(delete, State) ->
-	{stop, normal, State}.
+    {stop, normal, State}.
 
 %%--------------------------------------------------------------------
 %% Function: handle_info/2
@@ -114,7 +114,7 @@ handle_cast(delete, State) ->
 %%          {stop, Reason, State}            (terminate/2 is called)
 %%--------------------------------------------------------------------
 handle_info(timeout, State) ->
-	{stop, normal, State}.
+    {stop, normal, State}.
 
 %%--------------------------------------------------------------------
 %% Function: terminate/2
@@ -122,8 +122,8 @@ handle_info(timeout, State) ->
 %% Returns: any (ignored by gen_server)
 %%--------------------------------------------------------------------
 terminate(_Reason, _State) ->
-	sc_store:delete(self()),
-	ok.
+    sc_store:delete(self()),
+    ok.
 
 %%--------------------------------------------------------------------
 %% Func: code_change/3
@@ -131,21 +131,21 @@ terminate(_Reason, _State) ->
 %% Returns: {ok, NewState}
 %%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
-	{ok, State}.
+    {ok, State}.
 
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
 
 time_left(_StartTime, infinity) ->
-	infinity;
+    infinity;
 time_left(StartTime, LeaseTime) ->
-	Now = calendar:local_time(),
-	CurrentTime = calendar:datetime_to_gregorian_seconds(Now),
-	TimeElapsed = CurrentTime - StartTime,
-	case LeaseTime - TimeElapsed of
-		Time when Time =< 0 ->
-			0;
-		Time ->
-			Time * 1000
-	end.
+    Now = calendar:local_time(),
+    CurrentTime = calendar:datetime_to_gregorian_seconds(Now),
+    TimeElapsed = CurrentTime - StartTime,
+    case LeaseTime - TimeElapsed of
+	Time when Time =< 0 ->
+	    0;
+	Time ->
+	    Time * 1000
+    end.
