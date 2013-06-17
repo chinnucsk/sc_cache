42b26ebdd840c15ae9f8564aa2744cb4f557fe8f
Aaron <aaron.l.france@gmail.com>
2013-06-17 20:31:46 +0200

    Whitespace fix sc_element_sup.erl.

--- src/sc_element_sup.erl
+++ src/sc_element_sup.erl
@@ -17,15 +17,15 @@
 %% External exports
 %%--------------------------------------------------------------------
 -export([
-		 start_link/0,
-		 start_child/2
+	 start_link/0,
+	 start_child/2
         ]).
 
 %%--------------------------------------------------------------------
 %% Internal exports
 %%--------------------------------------------------------------------
 -export([
-		 init/1
+	 init/1
         ]).
 
 %%--------------------------------------------------------------------
@@ -45,10 +45,10 @@
 %% Description: Starts the supervisor
 %%--------------------------------------------------------------------
 start_link() ->
-	supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 start_child(Value, LeaseTime) ->
-	supervisor:start_child(?SERVER, [Value, LeaseTime]).
+    supervisor:start_child(?SERVER, [Value, LeaseTime]).
 
 %%====================================================================
 %% Server functions
@@ -60,11 +60,11 @@ start_child(Value, LeaseTime) ->
 %%          {error, Reason}   
 %%--------------------------------------------------------------------
 init([]) ->
-	Element = {sc_element, {sc_element, start_link, []},
-			   temporary, brutal_kill, worker, [sc_element]},
-	Children = [Element],
-	RestartStrategy = {simple_one_for_one, 0, 1},
-	{ok, {RestartStrategy, Children}}.
+    Element = {sc_element, {sc_element, start_link, []},
+	       temporary, brutal_kill, worker, [sc_element]},
+    Children = [Element],
+    RestartStrategy = {simple_one_for_one, 0, 1},
+    {ok, {RestartStrategy, Children}}.
 
 %%====================================================================
 %% Internal functions
