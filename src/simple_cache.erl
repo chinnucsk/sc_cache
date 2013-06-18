5ff0725c64d86e9de44a51e86d388886a50c5a46
Aaron <aaron.l.france@gmail.com>
2013-06-18 22:13:43 +0200

    Added docstrings to simple_cache.

--- src/simple_cache.erl
+++ src/simple_cache.erl
@@ -2,6 +2,11 @@
 
 -export([insert/2, delete/1, lookup/1, drop/0]).
 
+%% @doc
+%% insert/2 will place a new {Key, Value} pair into the simple_cache
+%% application, it will ignore whatever is already there by replacing
+%% the item currently in the cache.
+%% @spec insert(term(), value()) -> ok | {error, Reason}
 insert(Key, Value) ->
 	case sc_store:lookup(Key) of
 		{ok, Pid} ->
@@ -13,6 +18,10 @@ insert(Key, Value) ->
 			sc_store:insert(Key, Pid)
 	end.
 
+%% @doc
+%% lookup/1 will search for a given key in the simple_cache and return
+%% it if the item is there.
+%% @spec lookup(term) -> term() | {error, not_found}
 lookup(Key) ->
 	sc_event:lookup(Key),
 	try
@@ -23,6 +32,10 @@ lookup(Key) ->
 			 {error, not_found}
 	end.
 
+%% @doc
+%% delete/1 will delete a {Key, Value} pair in the simple_cache by
+%% indexing with the Key.
+%% @spec delete(term()) -> ok
 delete(Key) ->
 	sc_event:delete(Key),
 	case sc_store:lookup(Key) of
@@ -32,6 +45,10 @@ delete(Key) ->
 			ok
 	end.
 
+%% @doc
+%% drop/0 will drop the entire simple_cache store from memory and
+%% start fresh.
+%% @spec drop() -> ok
 drop() ->
     Children = sc_element_sup:list_children(),
     [gen_server:cast(Child, delete) || {_, Child, worker, _List} <- Children],
